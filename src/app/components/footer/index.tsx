dd72c1250f0346f3041ed2513c11dd6c1098969a
joy <joy.bhuiya@bjitgroup.com>
2024-01-16 17:27:06 +0600

    Till Mobile Footer Section

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -36,64 +36,77 @@ const open_sans = Open_Sans({ subsets: ["latin"] });
 const Footer = ({ leftMenu, rightMenu, about, copyright }: Props) => {
   return (
     <div className="footer">
-      <div className="footer__menu">
-        <div className="footer__menu-left">
-          {leftMenu?.map((menu, idx) => (
-            <Link key={idx} href={menu.link} className="footer__menu-name">
-              {menu.item}
-            </Link>
-          ))}
-        </div>
-        <div className="footer__menu-right">
-          {rightMenu?.map((menu, idx) => (
-            <Link key={idx} href={menu.link} className="footer__menu-name">
-              {menu.item}
-            </Link>
-          ))}
-        </div>
-      </div>
-      <div className="footer__details">
-        <div className="footer__about-us">
-          <div className="footer__logo">
-            <Image
-              src={about?.logo?.image}
-              alt="CleanList"
-              width={42}
-              height={43}
-            />
-            <span className="footer__about-us__logo-text">
-              {about?.logo?.name}
-            </span>
+      <div className="footer__wrapper">
+        <div className="footer__menu">
+          <div className="footer__menu-left">
+            {leftMenu?.map((menu, idx) => (
+              <Link key={idx} href={menu.link} className="footer__menu-name">
+                {menu.item}
+              </Link>
+            ))}
           </div>
-          <p className="footer__about-us__para">{about?.logo?.description}</p>
-        </div>
-        <div className="footer__question">
-          <h3 className="footer__question__header">{about?.question?.text}</h3>
-          <div className="footer__question__message">
-            <Image
-              src={about?.question?.icon}
-              alt="Message Logo"
-              width={40}
-              height={40}
-            />
-            <p className="footer__message__contact">
-              <span>{about?.question?.phone}</span> <br />
-              <span>{about?.question?.email}</span>
-            </p>
+          <div className="footer__menu-right">
+            {rightMenu?.map((menu, idx) => (
+              <Link key={idx} href={menu.link} className="footer__menu-name">
+                {menu.item}
+              </Link>
+            ))}
           </div>
         </div>
-        <div className="footer__social-media">
-          <h3 className={`footer__social-media__text ${open_sans.className}`}>
-            Follow us on
-          </h3>
-          {about?.followItems.map((item, idx) => (
-            <div key={idx} className="footer__social-media__logo">
-              <Image src={item.icon} alt={item.name} width={22} height={22} />
-              <span className="footer__social-media__logo-text">
-                {item.name}
+        <div className="footer__details">
+          <div className="footer__about-us">
+            <div className="footer__logo">
+              <Image
+                src={about?.logo?.image}
+                alt="CleanList"
+                width={42}
+                height={43}
+                className="footer__about-us__logo-image"
+              />
+              <span className="footer__about-us__logo-text">
+                {about?.logo?.name}
               </span>
             </div>
-          ))}
+            <p className="footer__about-us__description">
+              {about?.logo?.description}
+            </p>
+          </div>
+          <div className="footer__question">
+            <h3 className="footer__question__header">
+              {about?.question?.text}
+            </h3>
+            <div className="footer__question__message">
+              <Image
+                src={about?.question?.icon}
+                alt="Message Logo"
+                width={40}
+                height={40}
+              />
+              <p className="footer__message__contact">
+                <span>{about?.question?.phone}</span> <br />
+                <span>{about?.question?.email}</span>
+              </p>
+            </div>
+          </div>
+          <div className="footer__social-media">
+            <h3 className={`footer__social-media__text ${open_sans.className}`}>
+              Follow us on
+            </h3>
+            {about?.followItems.map((item, idx) => (
+              <div key={idx} className="footer__social-media__logo">
+                <Image
+                  src={item.icon}
+                  alt={item.name}
+                  width={22}
+                  height={22}
+                  className="footer__social-media__logo-image"
+                />
+                <span className="footer__social-media__logo-text">
+                  {item.name}
+                </span>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
       <div className={`footer__copyright ${open_sans}`}>{copyright}</div>
